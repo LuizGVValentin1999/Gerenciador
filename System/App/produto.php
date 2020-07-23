2bdacd095f28aff8fe1ebf0eee59b1d924e19fe7
Gabriel <gabriel.faria24@gmail.com>
2020-07-22 22:16:24 -0300

    Continuação do cadastro de produtos.

---
+++ System/App/produto.php
@@ -0,0 +1,12 @@
+<?php
+var_dump($_POST);
+if($_POST['cadastrar']){
+    cadastrar($_POST);
+
+}
+function cadastrar($post){
+
+    var_dump($post);
+
+}
+
--- System/View/Sistema/produto/cadastrarProduto.php
+++ System/View/Sistema/produto/cadastrarProduto.php
@@ -1,24 +1,74 @@
-<form method="post" action="#">
+<!-- Wrapper -->
+<div id="wrapper">
+	<!-- Main -->
+	<section class="wrapper">
+		<div class="inner">
+
+			<form method="POST" action="../../system/app/produto.php">
+	<div>
+	<input type="hidden" name="cadastrar" value="1" > 
+
 	<thead>
 		<tr>
-			<h2>cadastrar produtos</h2>
+			<h2>Adicionar produtos</h2>
 		</tr>
 	</thead>
 		<div class="row gtr-uniform">
 			<div class="col-6 col-12-xsmall">
+				<h4> Código: </h4>
+				<input type="text" name="codigo" id="codigo" value="" placeholder="Código" />
+			</div>
+			<div class="col-6 col-12-xsmall">
 				<h4> Nome do produto: </h4>
 				<input type="text" name="produto" id="produto" value="" placeholder="Produto" />
 			</div>
-			<div class="col-6 col-12-xsmall">
-				<h4> Nome do fornecedor: </h4>
-				<input type="text" name="fornecedor" id="fornecedor" value="" placeholder="Fornecedor" />
-			</div>
-			<div class="col-6 col-12-xsmall">
+
+			<div class="col-4 col-12-xsmall">
 				<h4> Tipo de medição: </h4>
-				<input type="text" name="tipo_medicao" id="tipo_medicao" value="" placeholder="Tipo de medição" />
+				<select name="tipo_medicao" id="demo-category">
+													<option value="">- Selecione o tipo de medição -</option>
+													<option value="1">Manufacturing</option>
+													<option value="1">Shipping</option>
+													<option value="1">Administration</option>
+													<option value="1">Human Resources</option>
+												</select>
+				
 			</div>
-			<div class="col-6 col-12-xsmall">
-				<h4> Descrição do produto: </h4>
-				<input type="text" name="descricao" id="descricao" value="" placeholder="Descrição" />
+
+			<div class="col-2 col-12-xsmall">
+				<br>
+			<a class="button" value="1" name="Botao" href="cadastrarProduto"> Adicionar </a>
 			</div>
-</form>             
+
+			<div class="col-4 col-12-xsmall">
+				<h4> Grupo: </h4>
+				<select name="selecao_grupo" id="grupo">
+													<option value="">- Selecione o grupo -</option>
+													<option value="1">Manufacturing</option>
+													<option value="1">Shipping</option>
+													<option value="1">Administration</option>
+													<option value="1">Human Resources</option>
+				</select>
+				
+			</div>
+
+			<div class="col-2 col-12-xsmall">
+			<br>
+			<a class="button" value="1" name="Botao" href="cadastrarProduto"> Adicionar </a>
+			</div>
+
+			<div class="col-12">
+			<h4> Descrição do produto: </h4>
+
+			<textarea name="descricao_produto" id="descricao_produto" placeholder="Descrição" rows="6"></textarea>
+			</div>
+
+			</div>
+			
+		
+			<button class="button" value="1" name="Botao" type="submit" style="float: right;margin-top: 20px; "> Salvar</button>
+
+			</form>  
+		</div>
+	</session> 
+</div>
